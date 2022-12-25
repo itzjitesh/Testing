35500f8b3a678bc502d929336a32244e62459cfe
Jitesh Singh <jitesh.0222@gmail.com>
2022-12-25 18:37:02 +0530

    update5

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 
-// app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
+    
+    res.render("index");
 
-    const __dirname = dirname(fileURLToPath(import.meta.url));
+    // const __dirname = dirname(fileURLToPath(import.meta.url));
 
-    res.sendFile(__dirname + "/public/index.html");
+    // res.sendFile(__dirname + "/public/index.html");
     // res.send("Hello World return again!");
     // console.log("trying");
     console.log("trying");
